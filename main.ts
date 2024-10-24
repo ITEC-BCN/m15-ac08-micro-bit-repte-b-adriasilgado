2a475998bfdcd5093154c8986ff62e826d9455af
adriasilgado <146826970+adriasilgado@users.noreply.github.com>
2024-10-24 17:30:27 +0200

    melodia per defecte, creada i canvi de bpm amb inclinació

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    music.play(music.stringPlayable("E B C5 A B G A F ", 120), music.PlaybackMode.UntilDone)
+})
+input.onButtonPressed(Button.B, function () {
+    music.play(music.stringPlayable("C G F A F G D C5 ", 120), music.PlaybackMode.UntilDone)
+})
 basic.forever(function () {
-	
+    music.changeTempoBy(input.rotation(Rotation.Pitch))
 })
